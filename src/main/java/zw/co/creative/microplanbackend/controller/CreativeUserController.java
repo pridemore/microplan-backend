ac6e6befc9fdc5d9da968dadb5df32b55b15318a
Pridemore.Vhiriri <prideviriri>
2022-10-05 13:38:15 +0200

    added view All Users

--- src/main/java/zw/co/creative/microplanbackend/controller/CreativeUserController.java
+++ src/main/java/zw/co/creative/microplanbackend/controller/CreativeUserController.java
@@ -9,13 +9,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import zw.co.creative.microplanbackend.domain.CreativeUser;
 import zw.co.creative.microplanbackend.domain.dto.CreativeUserDto;
+import zw.co.creative.microplanbackend.persistance.CreativeUserRepository;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Objects;
 
 @Controller
@@ -26,6 +30,9 @@ public class CreativeUserController {
 
     @Autowired
     private AuthenticatedEmployee authenticatedEmployee;
+
+    @Autowired
+    private CreativeUserRepository creativeUserRepository;
 
     private final RestTemplate restTemplate;
 
@@ -54,12 +61,26 @@ public class CreativeUserController {
             HttpEntity entity = new HttpEntity(headers);
             ResponseEntity<Object> exchange = restTemplate.postForEntity(resultURL,creativeUserDto,Object.class);
             log.info("Response from creating User------ : {}", exchange);
+
+            List<CreativeUser> creativeUser= creativeUserRepository.findAll();
+
+            model.addAttribute("creativeUsers",creativeUser);
             model.addAttribute("name",authenticatedEmployee.getAuthenticatedUser().getFirstName()+" "+authenticatedEmployee.getAuthenticatedUser().getLastName());
             return "pages/profile/view";
         }else{
             model.addAttribute("name",authenticatedEmployee.getAuthenticatedUser().getFirstName()+" "+authenticatedEmployee.getAuthenticatedUser().getLastName());
             return "pages/access/register";
         }
+    }
+
+    @RequestMapping(value = "/viewAll")
+    public String CREATIVEUSER(Model model){
+        List<CreativeUser> creativeUser= creativeUserRepository.findAll();
+
+        model.addAttribute("creativeUsers",creativeUser);
+        model.addAttribute("space", " ");
+        model.addAttribute("name",authenticatedEmployee.getAuthenticatedUser().getFirstName()+" "+authenticatedEmployee.getAuthenticatedUser().getLastName());
+        return "pages/profile/view";
     }
 
     private HttpHeaders extractHeaders() {
